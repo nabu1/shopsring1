2d10425732feabcad18966773b0c14900a147645
maciekkand <maciek.kand@gmail.com>
2019-01-02 19:47:59 +0100

    WiP Poprawne wyświetlanie fields w Shops Table

--- src/components/shopsTable/ShopsTable.vue
+++ src/components/shopsTable/ShopsTable.vue
@@ -6,7 +6,6 @@
         <b-table striped hover bordered :items="items" :fields="fields">
 
           <template slot="index" slot-scope="data">{{ data.index + 1 }}</template>
-          <template slot="shopName" slot-scope="data">{{ shopName }}</template>
           <template slot="total" slot-scope="data">{{ data.value * 100 }}</template>
 
           <span slot="google" slot-scope="data" v-html="data.value">{{ data.value }}</span>
@@ -28,98 +27,17 @@ export default {
   },
   computed: {
     items() {
+      console.log('this.$store.getters.getAllShops', this.$store.getters.getAllShops)
       return this.$store.getters.getAllShops
+    },
+    fields() {
+      console.log('this.$store.getters.getFields', this.$store.getters.getFields)
+      return this.$store.getters.getFields
     }
   },
   data() {
     return {
-      fields: [
-        'index',
-        {
-          key: "shopName",
-          label: "Sklep",
-          sortable: true
-        },
-        {
-          key: "address",
-          label: "Address",
-          sortable: true
-        },
-        {
-          key: "google",
-          label: "Google"
-        },
-        {
-          key: "jakd",
-          label: "JakD"
-        },
-        {
-          key: "gmaps",
-          label: "Gmaps"
-        },
-        {
-          key: "total",
-          label: "Total",
-          sortable: true,
-          variant: 'warning'
-        },
-        {
-          key: "chleb",
-          label: "Chleb",
-          sortable: true
-        },
-        {
-          key: "maslo",
-          label: "Masło",
-          sortable: true
-        },
-        {
-          key: "ser",
-          label: "Ser",
-          sortable: true
-        },
-        {
-          key: "jajko",
-          label: "Jajko",
-          sortable: true
-        },
-        {
-          key: "szynka",
-          label: "Szynka",
-          sortable: true
-        },
-        {
-          key: "kielbasa",
-          label: "Kiełbasa",
-          sortable: true
-        },
-        {
-          key: "cukier",
-          label: "Cukier",
-          sortable: true
-        },
-        {
-          key: "mleko",
-          label: "Mleko",
-          sortable: true
-        },
-        {
-          key: "smietana",
-          label: "Śmietana",
-          sortable: true
-        },
-        {
-          key: "mineralna",
-          label: "Mineralna",
-          sortable: true
-        },
-        {
-          key: "niematego",
-          sortable: true
-        }
-      ],
-      items: items()
-
+      items: items() || []
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -9,13 +9,104 @@ export const store = new Vuex.Store({
   state: {
     allShops: [],
     selectedShops: [],
-    homeGPS: {}
+    homeGPS: {},
+    fields: [
+      'index',
+      {
+        key: "shopName",
+        label: "Sklep",
+        sortable: true
+      },
+      {
+        key: "address",
+        label: "Adres",
+        sortable: true
+      },
+      {
+        key: "google",
+        label: "Google"
+      },
+      {
+        key: "jakd",
+        label: "JakD"
+      },
+      {
+        key: "gmaps",
+        label: "Gmaps"
+      },
+      {
+        key: "total",
+        label: "Total",
+        sortable: true,
+        variant: 'warning'
+      },
+      {
+        key: "chleb",
+        label: "Chleb",
+        sortable: true
+      },
+      {
+        key: "maslo",
+        label: "Masło",
+        sortable: true
+      },
+      {
+        key: "ser",
+        label: "Ser",
+        sortable: true
+      },
+      {
+        key: "jajko",
+        label: "Jajko",
+        sortable: true
+      },
+      {
+        key: "szynka",
+        label: "Szynka",
+        sortable: true
+      },
+      {
+        key: "kielbasa",
+        label: "Kiełbasa",
+        sortable: true
+      },
+      {
+        key: "cukier",
+        label: "Cukier",
+        sortable: true
+      },
+      {
+        key: "mleko",
+        label: "Mleko",
+        sortable: true
+      },
+      {
+        key: "smietana",
+        label: "Śmietana",
+        sortable: true
+      },
+      {
+        key: "mineralna",
+        label: "Mineralna",
+        sortable: true
+      },
+      {
+        key: "niematego",
+        sortable: true
+      }
+    ]
   },
   getters: {
     getAllShops(state) {
-      console.log('Tu getter "addAllShops"')
-      return state.allShops || []
+      //console.log('Tu getter "addAllShops"')
+      return state.allShops
     },
+
+    getFields(state) {
+      //console.log('Tu getter "addAllShops"')
+      return state.fields
+    },
+
     getCityShops(state) {
       return state.cityShops
     },
@@ -25,13 +116,13 @@ export const store = new Vuex.Store({
   },
   mutations: {
     ADD_ALL_SHOPS(state, payload) {
-      console.log('Tu mutation "ADD_ALL_SHOPS"')
+      //console.log('Tu mutation "ADD_ALL_SHOPS"')
       state.allShops = payload
       // console.log('state.allShops = ', state.allShops)
     },
     FIND_SELECTED_SHOPS(state, shopsInRadius) {
-      console.log('Tu mutations: findSelectedShops')
-      console.log('shopsInRadius', shopsInRadius)
+      //console.log('Tu mutations: findSelectedShops')
+      //console.log('shopsInRadius', shopsInRadius)
       state.allShops = shopsInRadius
     }
   },
@@ -44,7 +135,7 @@ export const store = new Vuex.Store({
         })
         .catch(err => console.log(err))
 
-      console.log('Tu action "addAllShops"')
+      //console.log('Tu action "addAllShops"')
     },
     findSelectedShops(context, homeData) {
       console.log('Tu: actions / findSelectedShops')
